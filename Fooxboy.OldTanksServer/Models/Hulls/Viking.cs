53b76b4a84e829b1cdcf3f964e59100be7e202f3
Fooxboy <Fooxboy@yandex.ru>
2019-10-16 22:00:53 +0300

    Добавил викинг

---
+++ Fooxboy.OldTanksServer/Models/Hulls/Viking.cs
@@ -0,0 +1,22 @@
+﻿using Fooxboy.OldTanksServer.Interfaces;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Fooxboy.OldTanksServer.Models.Hulls
+{
+    public class Viking : IHull
+    {
+        public long Id => 5;
+
+        public string Name => $"Викинг М{Level}";
+
+        public long? Level => null;
+
+        public List<long> Prices => new List<long>() { 600, 1800, 5400, 16200};
+
+        public List<long> Ranks => new List<long>() { 10, 12, 13, 14};
+
+        public List<long> Healths => new List<long>() { 100, 109, 125, 150};
+    }
+}
