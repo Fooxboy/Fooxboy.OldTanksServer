43eae638fdf56fa3d4dc9a46433a2d50bdd9e432
Fooxboy <Fooxboy@yandex.ru>
2019-10-16 22:07:03 +0300

    Добавил мамонта.

---
+++ Fooxboy.OldTanksServer/Models/Hulls/Mamont.cs
@@ -0,0 +1,22 @@
+﻿using Fooxboy.OldTanksServer.Interfaces;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Fooxboy.OldTanksServer.Models.Hulls
+{
+    public class Mamont : IHull
+    {
+        public long Id => 6;
+
+        public string Name => $"Мамонт М{Level}";
+
+        public long? Level => null;
+
+        public List<long> Prices => new List<long>() {750, 2250, 6800, 20400 };
+
+        public List<long> Ranks => new List<long>() { 12, 13, 14, 15};
+
+        public List<long> Healths => new List<long>() {130, 142, 165, 200 };
+    }
+}
