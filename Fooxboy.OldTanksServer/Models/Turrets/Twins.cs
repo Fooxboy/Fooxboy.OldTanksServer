8137c58645ff0cc1e377c481dfd1e1569c0c4633
Fooxboy <Fooxboy@yandex.ru>
2019-10-20 00:19:48 +0300

    Добавил твинс

---
+++ Fooxboy.OldTanksServer/Models/Turrets/Twins.cs
@@ -0,0 +1,22 @@
+﻿using Fooxboy.OldTanksServer.Interfaces;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Fooxboy.OldTanksServer.Models.Turrets
+{
+    public class Twins : ITurret
+    {
+        public long Id => 2;
+
+        public string Name => $"Твинс М{Level}";
+
+        public long? Level => null;
+
+        public List<long> Prices => new List<long> { 10, 50, 150, 500};
+
+        public List<long> Ranks => new List<long> { 4, 5, 7, 9};
+
+        public List<string> Params => new List<string> {"2;m0", "2;m1", "2;m2", "2;m3"};
+    }
+}
