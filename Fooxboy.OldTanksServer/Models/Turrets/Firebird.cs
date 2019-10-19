de42a720fea37ea9e27819d557f77b2f9e7db32f
Fooxboy <Fooxboy@yandex.ru>
2019-10-20 00:14:13 +0300

    Добавил огнемет

---
+++ Fooxboy.OldTanksServer/Models/Turrets/Firebird.cs
@@ -0,0 +1,22 @@
+﻿using Fooxboy.OldTanksServer.Interfaces;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Fooxboy.OldTanksServer.Models.Turrets
+{
+    public class Firebird : ITurret
+    {
+        public long Id => 1;
+
+        public string Name => $"Огнемёт М{Level}";
+
+        public long? Level => null;
+
+        public List<long> Prices => new List<long>() {5, 40, 120, 400 };
+
+        public List<long> Ranks => new List<long>() {3, 4, 6, 8 };
+
+        public List<string> Params => new List<string>() {"1;m0", "1;m1", "1;m2", "1;m3" };
+    }
+}
