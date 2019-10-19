4a72c3519677ee50b9a45f9c4adfc4d496d78296
Fooxboy <Fooxboy@yandex.ru>
2019-10-20 02:03:41 +0300

    Добавил рикошет

---
+++ Fooxboy.OldTanksServer/Models/Turrets/Ricochet.cs
@@ -0,0 +1,22 @@
+﻿using Fooxboy.OldTanksServer.Interfaces;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Fooxboy.OldTanksServer.Models.Turrets
+{
+    public class Ricochet : ITurret
+    {
+        public long Id => 7;
+
+        public string Name => $"Рикошет М{Level}";
+
+        public long? Level => null;
+
+        public List<long> Prices => new List<long>() { 655, 2000, 6000, 18000};
+
+        public List<long> Ranks => new List<long>() {13, 14, 15, 16 };
+
+        public List<string> Params => new List<string>() {"2;m4", "2;m5", "2;m6", "2;m7"  };
+    }
+}
