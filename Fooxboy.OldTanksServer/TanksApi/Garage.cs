2ffd9424753e7b009bc0b5808a2cb9e2af07c1a6
Fooxboy <Fooxboy@yandex.ru>
2019-10-21 17:32:13 +0300

    Добавил api для работы с гаражом

--- Fooxboy.OldTanksServer/TanksApi/Garage.cs
+++ Fooxboy.OldTanksServer/TanksApi/Garage.cs
@@ -1,7 +1,9 @@
-﻿using Fooxboy.OldTanksServer.Interfaces.TanksApi;
+﻿using Fooxboy.OldTanksServer.Database;
+using Fooxboy.OldTanksServer.Interfaces.TanksApi;
 using Fooxboy.OldTanksServer.Models;
 using System;
 using System.Collections.Generic;
+using System.Linq;
 using System.Text;
 
 namespace Fooxboy.OldTanksServer.TanksApi
@@ -10,22 +12,57 @@ namespace Fooxboy.OldTanksServer.TanksApi
     {
         public Models.Garage GetGarageFromId(long userId)
         {
-            throw new NotImplementedException();
+            using var db = new ServerDB();
+            return db.Garages.Single(g => g.UserId == userId);
         }
 
         public Models.Garage GetGarageFromNickname(string nickname)
         {
-            throw new NotImplementedException();
+            using var db = new ServerDB();
+            var user = db.Users.Single(u => u.Nickname == nickname);
+            return db.Garages.Single(g => g.UserId == user.UserId);
         }
 
         public bool Register(long userId)
         {
-            throw new NotImplementedException();
+            using var db = new ServerDB();
+            try
+            {
+                db.Garages.Add(new Models.Garage()
+                {
+                    UserId = userId,
+                    Colormaps = new List<Interfaces.IColormap>(),
+                    Crystalls = 0,
+                    CurrentColormap = null,
+                    CurrentHull = null,
+                    CurrentTurret = null,
+                    Hulls = new List<Interfaces.IHull>(),
+                    Numerics = new List<Interfaces.INumeric>(),
+                    Score = 0,
+                    Turrets = new List<Interfaces.ITurret>()
+                });
+                db.SaveChanges();
+                return true;
+            }catch(Exception e)
+            {
+                return false;
+            }
+            
         }
 
         public bool SetGarage(Models.Garage garage)
         {
-            throw new NotImplementedException();
+            using var db = new ServerDB();
+            try
+            {
+                var currentGarage = db.Garages.Single(g => g.UserId == garage.UserId);
+                currentGarage = garage;
+                db.SaveChanges();
+                return true;
+            }catch(Exception e)
+            {
+                return false;
+            }
         }
     }
 }
